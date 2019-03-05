7d055c5016be2f3c66d0db655f7ee07dc70f1c3b
Kitt Cymonne Japson <47837086+kittcy12@users.noreply.github.com>
2019-03-05 15:39:13 +0800

    Add files via upload
    Determining Pangram or Lipogram using Array of Characters

---
+++ pangram.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+	char input[100];
+	
+	int check=1,count=0;
+	gets(input);
+	int l=strlen(input);
+	strlwr(input);
+	for(int i=0;i<l;i++)
+		{
+			if(isalpha(input[i]))
+			
+				{
+					for(int j=i+1;j<l;j++)
+						{
+							if(input[i]==input[j])
+								{
+									check=0;
+									break;
+								}
+							else
+								{
+									check=1;
+								}
+							}	
+				if(check==1)
+					{
+						count++;
+					}
+				}
+			
+		}
+	if(count==26)
+		{
+			cout<<"Pangram";
+			
+		}
+	if(count==25)
+		{
+			cout<<"Lipogram";
+		}
+}
